594cf26b2fc17a61fd739afa76c33af5ebc18d3b
Jaewon <wodnjs3580@gmail.com>
2025-05-27 23:22:16 +0900

    feat: Spring Boot 기반의 백엔드 프로젝트 초기 설정 및 Gradle 구성 추가

---
+++ novel-prizes-backend/src/main/java/com/jaewon/novel_prizes_backend/NovelPrizesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jaewon.novel_prizes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelPrizesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelPrizesBackendApplication.class, args);
+	}
+
+}
---
+++ novel-prizes-backend/src/test/java/com/jaewon/novel_prizes_backend/NovelPrizesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaewon.novel_prizes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelPrizesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
